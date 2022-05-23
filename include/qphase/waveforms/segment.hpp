a67ec055b5c106e0b5ec9d27d8abd09a89603f36
Ben Baker <bakerb845@gmail.com>
2022-05-23 07:38:51 -0600

    forgot files

---
+++ include/qphase/waveforms/segment.hpp
@@ -0,0 +1,112 @@
+#ifndef QPHASE_WAVEFORMS_SEGMENT_HPP
+#define QPHASE_WAVEFORMS_SEGMENT_HPP
+#include <memory>
+#include <chrono>
+namespace QPhase::Waveforms
+{
+/// @brief This defines a waveform segment.  A waveform segment is a continuous
+///        segment of data as you would find in a data packet.
+template<class T = double>
+class Segment
+{
+public:
+    /// @name Constructors
+    /// @{
+
+    /// @brief Constructor.
+    Segment();
+    /// @brief Copy constructor.
+    /// @param[in] segment  The segment class from which to initialize this
+    ///                     class.
+    Segment(const Segment &segment);
+    /// @brief Move constructor.
+    /// @param[in,out] segment  The segment class from which to initialize this
+    ///                         class.  On exit, segment's behavior is
+    ///                         undefined.
+    Segment(Segment &&segment) noexcept;
+    /// @}
+
+    /// @name Operators
+    /// @{
+
+    /// @brief Copy assignment operator.
+    /// @param[in] segment  The segment to copy to this.
+    /// @result A deep copy of the input segment.
+    Segment& operator=(const Segment &segment);
+    /// @brief Move assignment operator.
+    /// @param[in] segment  The segment whose memory will be moved to this.
+    ///                     On exit, segment's behavior is undefined.
+    /// @result The memory from segment moved to this.
+    Segment& operator=(Segment &&segment) noexcept;
+    /// @}
+
+    /// @name Data
+    /// @{
+
+    /// @brief Sets the waveform dat afor the segment.
+    /// @param[in] data  The waveform data to set for this segment.
+    template<typename U> void setData(const std::vector<U> &data);
+    /// @param[in] nSamples  The number of samples in the data.
+    /// @param[in] data      The waveform data for this segment to set.
+    ///                      This is an array with dimension [nSamples].
+    template<typename U> void setData(const int nSamples, const U *data);
+    /// @result A pointer to the waveform data for this segment. 
+    const T *getDataPointer() const noexcept;
+    /// @result The waveform data for thsi segment.
+    std::vector<T> getData() const noexcept;
+ 
+    /// @result The number of samples in the segment.
+    [[nodiscard]] int getNumberOfSamples() const noexcept;
+    /// @}
+
+    /// @name Sampling Rate
+    /// @{
+
+    /// @brief Sets the sampling rate.
+    /// @param[in] samplingRate  The sampling rate in Hz.
+    /// @throws std::invalid_argument if the sampling rate is not positive.
+    void setSamplingRate(double samplingRate);
+    /// @result The sampling rate in Hz.
+    /// @throws std::runtime_error if \c haveSamplingRate() is false.
+    [[nodiscard]] double getSamplingRate() const;
+    /// @result The sampling period in seconds.
+    /// @throws std::runtime_error if \c haveSamplingRate() is false.
+    [[nodiscard]] double getSamplingPeriod() const;
+    /// @result The sampling period in microseconds.
+    /// @throws std::runtime_error if \c haveSamplingRate() is false.
+    [[nodiscard]] std::chrono::microseconds getSamplingPeriodInMicroSeconds() const;
+    /// @result True indicates the sampling rate was set.
+    [[nodiscard]] bool haveSamplingRate() const noexcept;
+    /// @}
+
+    /// @name Start and End Time
+    /// @{
+
+    /// @brief Sets the start time of the segment.
+    /// @param[in] startTime  The start time of the segment (UTC) in
+    ///                       seconds since the epoch.
+    void setStartTime(double startTime) noexcept;
+    /// @param[in] startTime  The start time of the segment (UTC) in
+    ///                       microseconds since the epoch.
+    void setStartTime(const std::chrono::microseconds &startTime) noexcept;
+    
+    /// @result The start time of the segment in microseconds since the epoch.
+    [[nodiscard]] std::chrono::microseconds getStartTime() const noexcept;
+    /// @result The end time of the segment in microseconds since the epoch.
+    [[nodiscard]] std::chrono::microseconds getEndTime() const noexcept;
+    /// @}
+
+    /// @name Destructors
+    /// @{
+
+    /// @brief Resets the class. 
+    void clear() noexcept;
+    /// @brief Destructor.
+    ~Segment();
+    /// @}
+private:
+    class SegmentImpl;
+    std::unique_ptr<SegmentImpl> pImpl;
+};
+}
+#endif
---
+++ src/waveforms/segment.cpp
@@ -0,0 +1,206 @@
+#include <cmath>
+#include <vector>
+#include <string>
+#include "qphase/waveforms/segment.hpp"
+
+using namespace QPhase::Waveforms;
+
+template<class T>
+class Segment<T>::SegmentImpl
+{
+public:
+    void updateEndTime()
+    {
+        mEndTime = mStartTime;
+        if (mSamplingRate > 0)
+        {
+            auto nSamples = static_cast<int64_t> (mWaveform.size());
+            if (nSamples > 0)
+            {
+                auto dtMuS = static_cast<int64_t> (std::round( 1000000
+                                                              /mSamplingRate ));
+                mEndTime = std::chrono::microseconds {(nSamples - 1)*dtMuS};
+            }
+        } 
+    }
+//private:
+    std::vector<T> mWaveform;
+    std::chrono::microseconds mStartTime{0};
+    std::chrono::microseconds mEndTime{0};
+    double mSamplingRate{0};
+};
+
+/// C'tor
+template<class T>
+Segment<T>::Segment() :
+    pImpl(std::make_unique<SegmentImpl> ())
+{
+}
+
+/// Copy c'tor
+template<class T>
+Segment<T>::Segment(const Segment &segment)
+{
+    *this = segment;
+}
+
+/// Move c'tor
+template<class T>
+Segment<T>::Segment(Segment &&segment) noexcept
+{
+    *this = std::move(segment);
+}
+
+/// Copy assignment
+template<class T>
+Segment<T>& Segment<T>::operator=(const Segment<T> &segment)
+{
+    if (&segment == this){return *this;}
+    pImpl = std::make_unique<SegmentImpl> (*segment.pImpl);
+    return *this;
+}
+
+/// Move assignment
+template<class T>
+Segment<T>& Segment<T>::operator=(Segment<T> &&segment) noexcept
+{
+    if (&segment == this){return *this;}
+    pImpl = std::move(segment.pImpl);
+    return *this;
+}
+
+/// Destructor
+template<class T>
+Segment<T>::~Segment() = default;
+
+/// Data
+template<class T>
+template<typename U>
+void Segment<T>::setData(const std::vector<U> &data)
+{
+    setData(data.size(), data.data());
+}
+
+template<class T>
+template<typename U>
+void Segment<T>::setData(const int nSamples, const U *__restrict__ data)
+{
+    if (nSamples < 1)
+    {
+        pImpl->mWaveform.resize(0);
+    }
+    else
+    {
+        if (data == nullptr){throw std::invalid_argument("data is NULL");}
+        // Copy data
+        pImpl->mWaveform.resize(nSamples);
+        T *__restrict__ waveformPtr = pImpl->mWaveform.data();
+        std::copy(data, data + nSamples, waveformPtr);
+    }
+    pImpl->updateEndTime();
+}
+
+template<class T>
+const T* Segment<T>::getDataPointer() const noexcept
+{
+    return pImpl->mWaveform.data();
+}
+
+template<class T>
+std::vector<T> Segment<T>::getData() const noexcept
+{
+    return pImpl->mWaveform;
+}
+
+/// Number of samples
+template<class T>
+int Segment<T>::getNumberOfSamples() const noexcept
+{
+    return static_cast<int> (pImpl->mWaveform.size());
+}
+
+/// Sampling rate 
+template<class T>
+void Segment<T>::setSamplingRate(const double samplingRate)
+{
+    if (samplingRate <= 0)
+    {
+        throw std::invalid_argument("Sampling rate must be positive");
+    }
+    pImpl->mSamplingRate = samplingRate;
+    pImpl->updateEndTime();
+}
+
+template<class T>
+double Segment<T>::getSamplingPeriod() const
+{
+    return 1./getSamplingRate();
+}
+
+template<class T>
+std::chrono::microseconds Segment<T>::getSamplingPeriodInMicroSeconds() const
+{
+    auto dt = getSamplingPeriod();
+    std::chrono::microseconds
+        dtMuS{static_cast<int64_t> (std::round(1000000*dt))};
+    return dtMuS;
+}
+
+template<class T>
+double Segment<T>::getSamplingRate() const
+{
+    if (!haveSamplingRate())
+    {
+        throw std::runtime_error("Sampling rate not set");
+    }
+    return pImpl->mSamplingRate;
+}
+
+template<class T>
+bool Segment<T>::haveSamplingRate() const noexcept
+{
+    return (pImpl->mSamplingRate > 0);
+}
+
+/// Start time
+template<class T>
+void Segment<T>::setStartTime(const double startTime) noexcept
+{
+    auto it0 = static_cast<int64_t> (std::round(startTime*1000000));
+    std::chrono::microseconds t0{it0};
+    setStartTime(t0);
+}
+
+template<class T>
+void Segment<T>::setStartTime(const std::chrono::microseconds &t0) noexcept
+{
+    pImpl->mStartTime = t0;
+    pImpl->updateEndTime();
+} 
+
+template<class T>
+std::chrono::microseconds Segment<T>::getStartTime() const noexcept
+{
+    return pImpl->mStartTime;
+}
+
+template<class T>
+std::chrono::microseconds Segment<T>::getEndTime() const noexcept
+{
+    return pImpl->mEndTime;
+}
+
+///--------------------------------------------------------------------------///
+///                          Template Instantiation                          ///
+///--------------------------------------------------------------------------///
+template class QPhase::Waveforms::Segment<double>;
+template class QPhase::Waveforms::Segment<float>;
+template void QPhase::Waveforms::Segment<double>::setData(const std::vector<double> &);
+template void QPhase::Waveforms::Segment<double>::setData(const std::vector<float> &);
+template void QPhase::Waveforms::Segment<float>::setData(const std::vector<double> &);
+template void QPhase::Waveforms::Segment<float>::setData(const std::vector<float> &);
+template void QPhase::Waveforms::Segment<double>::setData(int, const double *);
+template void QPhase::Waveforms::Segment<double>::setData(int, const float *);
+template void QPhase::Waveforms::Segment<float>::setData(int, const double *); 
+template void QPhase::Waveforms::Segment<float>::setData(int, const float *); 
+
